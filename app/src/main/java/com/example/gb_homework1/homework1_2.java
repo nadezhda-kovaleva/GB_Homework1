142437fbc784d0c8e9320e4f6dcae1e384049159
Nadezhda <vist@ro.ru>
2021-05-27 18:07:00 +0300

    Homework 1

---
+++ app/src/main/java/com/example/gb_homework1/homework1_2.java
@@ -0,0 +1,14 @@
+package com.example.gb_homework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homework1_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework1_2);
+    }
+}
